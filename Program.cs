077ba974f1127d486591b73541ddc015bcde07b2
et120 <e.trotter120@gmail.com>
2023-12-12 09:19:40 -0800

    sayhello

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Elizabeth Trotter
+
+// Date Revised: 9/6/2023
+
+// Exercise: Mini Challenge # 1 - Say Hello
+
+// Brief Description: Create a brand new console project where you are asked your name & then print out the following "Hello <name entered>"
+
+// Peer Reviewer: Waqas Dad
+
+Console.Clear();
+
+Console.WriteLine("Hi friend what is your first name?");
+
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName);
